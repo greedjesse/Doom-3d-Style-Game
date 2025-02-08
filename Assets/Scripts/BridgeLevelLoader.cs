6c2a613abcefb3bcb44f04958debdcef2037836d
greedjesse <67365952+greedjesse@users.noreply.github.com>
2025-02-08 14:21:10 +0800

    Added arrow fill effect.

--- Assets/Scripts/BridgeLevelLoader.cs
+++ Assets/Scripts/BridgeLevelLoader.cs
@@ -6,6 +6,9 @@ public class BridgeLevelLoader : MonoBehaviour
     [SerializeField] private PlayerMovement player;
     [SerializeField] private LevelLoader levelLoader;
 
+    [SerializeField] private Material arrowMat;
+    private int _arrowFillRatioID;
+
     [SerializeField] private Vector3 playerSpawnPosition;
     [SerializeField] private float cameraDirection;
     [SerializeField] private int destination;
@@ -20,6 +23,8 @@ public class BridgeLevelLoader : MonoBehaviour
 
     void Start()
     {
+        _arrowFillRatioID = Shader.PropertyToID("_FillRatio");
+        
         _timeHeld = 0.0f;
         _transferred = false;
         collided = false;
@@ -33,11 +38,13 @@ public class BridgeLevelLoader : MonoBehaviour
             _timeHeld += Time.deltaTime;
             _timeHeld = Math.Min(timeNeededToHeld, _timeHeld);
         }
-        else
+        else if (_timeHeld < timeNeededToHeld)
         {
             _timeHeld -= Time.deltaTime;
             _timeHeld = Math.Max(0.0f, _timeHeld);
         }
+        
+        arrowMat.SetFloat(_arrowFillRatioID, CircleFunc(1.0f, 0.0f, 1.0f, _timeHeld / timeNeededToHeld * 0.6f));
     }
 
     private void OnTriggerEnter(Collider other)
@@ -65,5 +72,15 @@ public class BridgeLevelLoader : MonoBehaviour
             _transferred = true;
             StartCoroutine(levelLoader.LoadLevel(destination, playerSpawnPosition, cameraDirection));
         }
+    }
+
+    private float ParabolicFunc(float a, float b, float c, float x)
+    {
+        return a * Mathf.Pow((x + b), 2) + c;
+    }
+
+    private float CircleFunc(float a, float b, float r, float x)
+    {
+        return Mathf.Sqrt(Mathf.Pow(r, 2) - Mathf.Pow(x - a, 2) + b);
     }
 }
